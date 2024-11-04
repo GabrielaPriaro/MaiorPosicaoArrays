1324dfd21f7b210d10de5f95f723c717f7f70021
Gabriela <gabriela.priaro@gmail.com>
2024-11-04 11:55:10 +0000

    Implementaçao para mostrar maior valor e posiçao do maior valor, entre os numeros digitados pelo usuário.

--- MaiorPosiçaoArrays/Program.cs
+++ MaiorPosiçaoArrays/Program.cs
@@ -4,13 +4,46 @@
 //o maior número do vetor (supor não haver empates). Mostrar também a posição do maior elemento,
 //considerando a primeira posição como 0 (zero). 
 
+using System.Globalization;
+
 namespace MaiorPosiçaoArrays
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n, posicaoMaiorValor;
+            double maiorValor;
+
+            posicaoMaiorValor = 0;
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Quanto numeros voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+
+            maiorValor = vet[0];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite um numero: ");
+                vet[i] = double.Parse(Console.ReadLine(), CI);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (vet[i] > maiorValor)
+                {
+                    maiorValor = vet[i];
+                    posicaoMaiorValor = i;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"MAIOR VALOR = {maiorValor.ToString("F1", CI)}");
+            Console.WriteLine($"POSICAO DO MAIOR VALOR = {posicaoMaiorValor}");
         }
     }
 }
